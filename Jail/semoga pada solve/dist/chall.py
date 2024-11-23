42ae4b2de6e06e3bee19bdc9923539ef52848f9d
Muhammad Zahran <muhammadzahran@apps.ipb.ac.id>
2024-11-23 08:18:19 +0700

    perbaikan dikit di chall pyjail

--- Jail/semoga pada solve/dist/chall.py
+++ Jail/semoga pada solve/dist/chall.py
@@ -2,8 +2,8 @@
 
 apa_ini = input('> ')
 
-blacklist = ['breakpoint', 'print', 'input', 'eval', 'exec', 'open', 'import', 'globals', 'locals', 'builtins', 'dir', 'os', 'sys', 'attr', '_', '+', 'system', 'subprocess']
-if any([c for c in blacklist if c in apa_ini]):
+blacklist = ['breakpoint', 'print', 'input', 'eval', 'exec', 'open', 'import', 'globals', 'locals', 'builtins', 'dir', 'os', 'sys', 'attr', '_', '"', "'", 'system', 'subprocess']
+if not apa_ini.isascii() or not apa_ini.isprintable() or any([c for c in blacklist if c in apa_ini]):
     print('gaboleh begitu bang hekel')
     exit()
 
